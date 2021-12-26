b4c461b758321b2e50284fe73ba4beea52867943
boldbeak <boldbeakdev@gmail.com>
2021-12-25 22:55:42 -0600

    page single element adds to container div based on dimensions defined in css

---
+++ script.js
@@ -0,0 +1,9 @@
+//--- TOP: Etch-a-Sketch ---
+
+const cont = document.querySelector('.container');
+
+let newSquare = document.createElement('div');
+
+newSquare.classList.add('gridSquare');
+
+cont.appendChild(newSquare);
