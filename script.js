42f7dddf360fad410f6aee015c9d69ad485c8f52
boldbeak <boldbeakdev@gmail.com>
2022-01-05 12:48:03 -0600

    Change square id to string

--- script.js
+++ script.js
@@ -4,17 +4,18 @@ const cont = document.querySelector('.container');
 const contHeight = cont.clientHeight;
 const contWidth = cont.clientWidth;
 console.log(contHeight);
+
+// number of squares per side in grid
 let num = 16;
 
 for (let i = 0; i < num * num; i++) {
-  let sqId = i + 1;
+  let sqId = `sqNum${i + 1}`;
   let sqHeight = (contHeight / num) - 2;
   let sqWidth = (contWidth / num) - 2;
   const newSquare = document.createElement("div");
+
   newSquare.classList.add("gridSquare");
   newSquare.setAttribute("id", sqId);
   newSquare.setAttribute("style", `height: ${sqHeight}px; width: ${sqWidth}px`)
-  // newSquare.setAttribute("height", sqHeight + "px");
-  // newSquare.setAttribute("width", sqWidth + "px");
   cont.appendChild(newSquare);
 }
