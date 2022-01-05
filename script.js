2b4db35d7d57f702935a1fc2d2a4f3cfef300fcf
boldbeak <boldbeakdev@gmail.com>
2022-01-05 12:43:46 -0600

    Update js to create grid of squares with set height and width

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 //--- TOP: Etch-a-Sketch ---
 
 const cont = document.querySelector('.container');
-
+const contHeight = cont.clientHeight;
+const contWidth = cont.clientWidth;
+console.log(contHeight);
 let num = 16;
 
-for (let i = 0; i < num; i++) {
+for (let i = 0; i < num * num; i++) {
   let sqId = i + 1;
-  let sqHeight = (90 / num) + "vh";
-  let sqWidth = (90 / num) + "vw";
+  let sqHeight = (contHeight / num) - 2;
+  let sqWidth = (contWidth / num) - 2;
   const newSquare = document.createElement("div");
   newSquare.classList.add("gridSquare");
   newSquare.setAttribute("id", sqId);
-  // newSquare.setAttribute("height", sqHeight);
-  // newSquare.setAttribute("width", sqWidth);
+  newSquare.setAttribute("style", `height: ${sqHeight}px; width: ${sqWidth}px`)
+  // newSquare.setAttribute("height", sqHeight + "px");
+  // newSquare.setAttribute("width", sqWidth + "px");
   cont.appendChild(newSquare);
 }
