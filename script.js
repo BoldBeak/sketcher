966910ef6045a9f81c82c85ad4edc4eedf4d5904
boldbeak <boldbeakdev@gmail.com>
2022-01-06 14:40:40 -0600

    Add loop to create grid units, Update css to display as grid

--- script.js
+++ script.js
@@ -8,14 +8,45 @@ let rowCount = 16;
 let columnCount = 16;
 
 for (let i = 0; i < rowCount; i++) {
-  let row = document.createElement("div");
+  const row = document.createElement("div");
   let rowNum = "rowNum" + i;
-  let rowHeight = (contHeight / rowCount) - 2;
+
+  // rowHeight subtracts 2 to offset width of border (top + bottom);
+  const rowHeight = (contHeight / rowCount) - 2;
 
   row.classList.add(`flexRow`, rowNum);
   row.setAttribute("style", `height: ${rowHeight}px; flex: 1 1 0`);
   cont.appendChild(row);
 }
+
+
+
+// function makeColumns(columnCount) {
+  let colContainer = document.querySelectorAll('.flexRow');
+
+  // for each row (colContainer) ----->>
+  for (let j = 0; j < colContainer.length; j++) {
+
+    // add the number of divs specified by columnCount
+    for (let k = 0; k < columnCount; k++) {
+
+      // get height and width of each row
+      const colHeight = colContainer[j].clientHeight;
+      const colWidth = colContainer[j].clientWidth;
+
+      const columnUnit = document.createElement("div");
+      let columnNum = "colNum" + j;
+
+      // height and width offset by 2 for borders
+      const columnUnitWidth = (colWidth / columnCount) - 2;
+      const columnUnitHeight = colHeight - 2
+
+      columnUnit.classList.add("gridUnit", columnNum);
+      columnUnit.setAttribute("style", `height: ${columnUnitHeight}; width: ${columnUnitWidth}; flex: 1 1 0`);
+      colContainer[j].appendChild(columnUnit);
+    }
+  }
+//}
 
 // number of squares per side in grid
 // let num = 16;
