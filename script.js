2bd1227705ee3fa5ddda3e8f8e9d5961d34cdbbf
boldbeak <boldbeakdev@gmail.com>
2022-01-06 12:52:43 -0600

    Rework js to fill container with rows, Add flexRow class to css

--- script.js
+++ script.js
@@ -3,19 +3,32 @@
 const cont = document.querySelector('.container');
 const contHeight = cont.clientHeight;
 const contWidth = cont.clientWidth;
-console.log(contHeight);
+
+let rowCount = 16;
+let columnCount = 16;
+
+for (let i = 0; i < rowCount; i++) {
+  let row = document.createElement("div");
+  let rowNum = "rowNum" + i;
+  let rowHeight = (contHeight / rowCount) - 2;
+
+  row.classList.add(`flexRow`, rowNum);
+  row.setAttribute("style", `height: ${rowHeight}px`);
+  cont.appendChild(row);
+}
 
 // number of squares per side in grid
-let num = 16;
+// let num = 16;
 
-for (let i = 0; i < num * num; i++) {
-  let sqId = `sqNum${i + 1}`;
-  let sqHeight = (contHeight / num) - 2;
-  let sqWidth = (contWidth / num) - 2;
-  const newSquare = document.createElement("div");
+// for (let i = 0; i < num * num; i++) {
+//   let sqId = `sqNum${i + 1}`;
+//   let sqHeight = (contHeight / num) - 2;
+//   let sqWidth = (contWidth / num) - 2;
+//   const newSquare = document.createElement("div");
 
-  newSquare.classList.add("gridSquare");
-  newSquare.setAttribute("id", sqId);
-  newSquare.setAttribute("style", `height: ${sqHeight}px; width: ${sqWidth}px`)
-  cont.appendChild(newSquare);
-}
+//   newSquare.classList.add("gridSquare");
+//   newSquare.setAttribute("id", sqId);
+  
+//   newSquare.setAttribute("style", `height: ${sqHeight}px; width: ${sqWidth}px`)
+//   cont.appendChild(newSquare);
+// }
