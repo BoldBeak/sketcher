5e09661a9e3b87fa2a495567737fc4ed6b035010
boldbeak <boldbeakdev@gmail.com>
2022-07-19 14:22:00 -0500

    Reconfigure grid creation into function, reconfigure setSize and assign event to resize button

--- script.js
+++ script.js
@@ -1,56 +1,53 @@
 //--- TOP: Etch-a-Sketch ---
 
-
 //--- Begin Initial Grid Creation ---
 const cont = document.querySelector('.container');
 const contHeight = cont.clientHeight;
 const contWidth = cont.clientWidth;
 
-let rowCount = setSize();
+let rowCount = 16;
 let columnCount = rowCount;
 
-for (let i = 0; i < rowCount; i++) {
-  const row = document.createElement("div");
-  let rowNum = "rowNum" + i;
+createGrid(16, 16);
 
-  // rowHeight subtracts 2 to offset width of border (top + bottom);
-  const rowHeight = (contHeight / rowCount) - 2;
+function createGrid(rowCount, columnCount) {
+  for (let i = 0; i < rowCount; i++) {
+    const row = document.createElement("div");
+    let rowNum = "rowNum" + i;
 
-  row.classList.add(`flexRow`, rowNum);
-  row.setAttribute("style", `height: ${rowHeight}px; flex: 1 1 0`);
-  cont.appendChild(row);
-}
+    // rowHeight subtracts 2 to offset width of border (top + bottom);
+    const rowHeight = (contHeight / rowCount) - 2;
 
-let colContainer = document.querySelectorAll('.flexRow');
-
-// for each row (colContainer) ----->>
-for (let j = 0; j < colContainer.length; j++) {
-
-  // add the number of divs specified by columnCount
-  for (let k = 0; k < columnCount; k++) {
-    // get height and width of each row
-    const colHeight = colContainer[j].clientHeight;
-    const colWidth = colContainer[j].clientWidth;
-
-    const columnUnit = document.createElement("div");
-    let columnNum = "colNum" + k;
-
-    // width offset by 2 for borders
-    const columnUnitWidth = (colWidth / columnCount) - 2;
-
-    columnUnit.classList.add("gridUnit", columnNum);
-    columnUnit.setAttribute("style", `height: ${colHeight}; width: ${columnUnitWidth}; flex: 1 1 0`);
-    colContainer[j].appendChild(columnUnit);
+    row.classList.add(`flexRow`, rowNum);
+    row.setAttribute("style", `height: ${rowHeight}px; flex: 1 1 0`);
+    cont.appendChild(row);
   }
-}
 
-// let gridUnit = document.querySelectorAll("div.gridUnit");
+  let colContainer = document.querySelectorAll('.flexRow');
 
-// gridUnit.forEach(gridSquare => {
-//   gridSquare.addEventListener('mouseover', () => {
-//     gridSquare.classList.add("mark");
-//   });
-// });
+  // for each row (colContainer) ----->>
+  for (let j = 0; j < colContainer.length; j++) {
+
+    // add the number of divs specified by columnCount
+    for (let k = 0; k < columnCount; k++) {
+      // get height and width of each row
+      const colHeight = colContainer[j].clientHeight;
+      const colWidth = colContainer[j].clientWidth;
+
+      const columnUnit = document.createElement("div");
+      let columnNum = "colNum" + k;
+
+      // width offset by 2 for borders
+      const columnUnitWidth = (colWidth / columnCount) - 2;
+
+      columnUnit.classList.add("gridUnit", columnNum);
+      columnUnit.setAttribute("style", `height: ${colHeight}; width: ${columnUnitWidth}; flex: 1 1 0`);
+      colContainer[j].appendChild(columnUnit);
+    }
+  }
+  };
+
+
 // --- End Grid Creation ---
 
 // --- Begin Button Creation ---
@@ -70,11 +67,13 @@ const docBody = document.querySelector('body');
 docBody.insertBefore(btnCont, cont);
 
 document.querySelector('.reset').textContent = "Clear Grid";
-document.querySelector('.resize').textContent = "Resize Grid";
+// document.querySelector('.resize').textContent = "Resize Grid";
 document.querySelector('.multiColor').textContent = "Multi-Color Fun!";
 document.querySelector('.fade').textContent = "Fade Out";
 
-
+clearButton = document.querySelector('.resize');
+clearButton.textContent = "Resize Grid";
+clearButton.addEventListener('click', setSize);
 // --- Begin Button Action Functions ---
 
 let gridUnit = document.querySelectorAll("div.gridUnit");
@@ -86,9 +85,9 @@ gridUnit.forEach(gridSquare => {
 });
 
 function setSize() {
-  const initialSize = 16;
-  return initialSize;
-}
+  rowCount = prompt("Choose the number of squares per row", "16");
+  createGrid(rowCount, columnCount);
+  };
 
 
 
